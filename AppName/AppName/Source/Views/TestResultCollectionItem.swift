101c2f245883feca77007558ba112d87c34abf92
Aglaia Feli <aglaiafeli@gmail.com>
2018-07-06 17:26:56 +0200

    Make the header and footer views work within the collection view

---
+++ AppName/AppName/Source/Views/TestResultCollectionItem.swift
@@ -0,0 +1,13 @@
+//
+//  TestResultCollectionItem.swift
+//  AppName
+//
+//  Created by Aglaia on 7/6/18.
+//  Copyright © 2018 Aglaia Feli. All rights reserved.
+//
+
+import UIKit
+
+class TestResultCollectionItem: UICollectionViewCell {
+    
+}
